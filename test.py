8435575829b1b8033a168ca1f6ec1ce9a2a4692a
ericevenchick <eric.evenchick@gmail.com>
2011-07-26 17:25:03 -0400

    Added test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+
+class TestApp(App):
+    def build(self):
+        b = Button(text="hello")
+	l = BoxLayout(orientation='vertical')
+	l.add_widget(b)
+	return l
+
+TestApp().run()
